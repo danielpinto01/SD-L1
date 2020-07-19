506aeb12491fe19a2a0e9f4168930a5c2e2a56b8
Andrés Felipe Flórez <36776473+florez18399@users.noreply.github.com>
2020-07-18 23:20:26 -0500

    Cambio 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Pinto mk');
+  res.send('<h1>Pinto re contra re MK</h1>');
 });
 
 app.listen(3000, function () {
